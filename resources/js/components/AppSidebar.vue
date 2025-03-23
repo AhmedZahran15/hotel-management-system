5ecc86f4d58593a1d80acd531fa1d12840849e31
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-03-23 19:41:02 +0200

    added manage floor to dashboard

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid , CableCar } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -14,20 +14,25 @@ const mainNavItems: NavItem[] = [
         href: '/dashboard',
         icon: LayoutGrid,
     },
-    { 
-        title: 'Manage Managers', 
-        href: '/manage-managers', 
-        icon: LayoutGrid, 
+    {
+        title: 'Manage Managers',
+        href: '/manage-managers',
+        icon: LayoutGrid,
     },
-    { 
-        title: 'Manage Receptionists', 
-        href: '/manage-receptionists', 
-        icon: LayoutGrid, 
+    {
+        title: 'Manage Receptionists',
+        href: '/manage-receptionists',
+        icon: LayoutGrid,
     },
-    { 
-        title: 'Manage Clients', 
-        href: '/manage-clients', 
-        icon: LayoutGrid, 
+    {
+        title: 'Manage Clients',
+        href: '/manage-clients',
+        icon: LayoutGrid,
+    },
+    {
+        title: 'Manage Floors',
+        href: route("floors.index"),
+        icon: CableCar,
     },
 ];
 
