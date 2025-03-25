24cea05562fa717f51d09dd02e2c6147b33d3f8a
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-03-25 04:15:24 +0200

    feat: add client approval functionality with notification

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\ClientResource;
 use App\Models\Client;
 use App\Models\Phone;
 use App\Models\User;
+use App\Notifications\ClientApprovedNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
@@ -20,16 +21,14 @@ class ClientController extends Controller
      */
     public function index()
     {
-        if(Auth::user() && Auth::user()->hasAnyRole(["admin","manager"])) {
-            return Inertia::render("Admin/ManageClients",["clients"=> ClientResource::collection(Client::with("user",'phones')->paginate(10))]);
-        }
-        else if(Auth::user()->hasRole("receptionist")) {
-            return Inertia::render("Admin/ManageClients",[
-                "clients"=> ClientResource::collection(Client::with("user")->whereNull("approved_by")->paginate(10)),
-                "approved_clients"=> ClientResource::collection(Client::with('user','phones')->where("approved_by",Auth::id())->get())
+        if (Auth::user() && Auth::user()->hasAnyRole(["admin", "manager"])) {
+            return Inertia::render("Admin/ManageClients", ["clients" => ClientResource::collection(Client::with("user", 'phones')->paginate(10))]);
+        } else if (Auth::user()->hasRole("receptionist")) {
+            return Inertia::render("Admin/ManageClients", [
+                "clients" => ClientResource::collection(Client::with("user")->whereNull("approved_by")->paginate(10)),
+                "approved_clients" => ClientResource::collection(Client::with('user', 'phones')->where("approved_by", Auth::id())->get())
             ]);
-        }
-        else {
+        } else {
             return response()->json(['message' => 'Unauthorized'], 403);
         }
     }
@@ -48,49 +47,49 @@ class ClientController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            "name"=>["required","string","min:3"],
-            "avatar_image"=>["image","mimes:jpg,jpeg","max:2048"],
-            "country"=>["required","string"],
-            "gender"=>["required","string","in:male,female"],
-            "email"=>["required","string","email","unique:users",],
-            "password"=>["required","string","min:8","confirmed"],
+            "name" => ["required", "string", "min:3"],
+            "avatar_image" => ["image", "mimes:jpg,jpeg", "max:2048"],
+            "country" => ["required", "string"],
+            "gender" => ["required", "string", "in:male,female"],
+            "email" => ["required", "string", "email", "unique:users",],
+            "password" => ["required", "string", "min:8", "confirmed"],
             'phone' => ['sometimes', 'string', 'regex:/^\+?[0-9]{7,}$/'],
         ]);
         //handling adding user:
         $user = User::create([
-            "name"=> $request->name,
-            "email"=> $request->email,
-            "password"=> Hash::make($request->password),
-            "user_type"=>"client",
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password),
+            "user_type" => "client",
         ]);
 
         $user->assignRole("client");
 
         //handle profile picture
         $filename = null;
-        if($request->file("avatar_image")){
+        if ($request->file("avatar_image")) {
             $storagePath = config('app.client_avatar_storage_path');
             $extension = $request->file('avatar_image')->getClientOriginalExtension();
             $filename = "client-{$user->id}.{$extension}";
-            $request->file("avatar_image")->storeAs($storagePath,$filename,"local"); // NeedEdit: add env var
+            $request->file("avatar_image")->storeAs($storagePath, $filename, "local"); // NeedEdit: add env var
         }
 
         //handle client creation
         $client = Client::create([
-            "name"=> $request->name,
-            "country"=> $request->country,
-            "gender"=> $request->gender,
-            "user_id"=> $user->id,
+            "name" => $request->name,
+            "country" => $request->country,
+            "gender" => $request->gender,
+            "user_id" => $user->id,
             "img_name" => $filename ?? "default.jpg"
         ]);
         //handle phone;
-        if($request->phone){
+        if ($request->phone) {
             $hpone = Phone::create([
-                "phone"=> $request->phone,
-                "client_id"=>$client->id
+                "phone" => $request->phone,
+                "client_id" => $client->id
             ]);
         }
-        return back()->with("success","Client created successfully");
+        return back()->with("success", "Client created successfully");
     }
 
     /**
@@ -98,8 +97,8 @@ class ClientController extends Controller
      */
     public function show($client)
     {
-        $client = new ClientResource(Client::findOrFail( $client ));
-        return Inertia::render("", ["client"=> $client]);
+        $client = new ClientResource(Client::findOrFail($client));
+        return Inertia::render("", ["client" => $client]);
     }
 
     /**
@@ -107,8 +106,8 @@ class ClientController extends Controller
      */
     public function edit(string $id)
     {
-        $client = new ClientResource(Client::with("user")->findOrFail( $id ));
-        return Inertia::render("",["employee"=> $client]);
+        $client = new ClientResource(Client::with("user")->findOrFail($id));
+        return Inertia::render("", ["employee" => $client]);
     }
 
     /**
@@ -117,79 +116,76 @@ class ClientController extends Controller
     public function update(Request $request, string $id)
     {
 
-        $client = Client::with("user")->findOrFail( $id );
+        $client = Client::with("user")->findOrFail($id);
         $request->validate([
-            "name"=>["required","string","min:3"],
-            "country"=>["required","string"],
-            "gender"=>["required","string","in:male,female"],
-            "email"=>["required","string",Rule::unique("users")->ignore($client->user->id)],
-            "password"=>["sometimes","string","min:8","confirmed"],
-            "avatar_image"=>["sometimes","image","mimes:jpg,jpeg","max:2048"],
+            "name" => ["required", "string", "min:3"],
+            "country" => ["required", "string"],
+            "gender" => ["required", "string", "in:male,female"],
+            "email" => ["required", "string", Rule::unique("users")->ignore($client->user->id)],
+            "password" => ["sometimes", "string", "min:8", "confirmed"],
+            "avatar_image" => ["sometimes", "image", "mimes:jpg,jpeg", "max:2048"],
         ]);
 
-        $client->update(["name"=>$request->name,"country"=>$request->country, "gender"=>$request->gender]);
-        $client->user->update(["email"=>$request->email]);
+        $client->update(["name" => $request->name, "country" => $request->country, "gender" => $request->gender]);
+        $client->user->update(["email" => $request->email]);
 
-        if($request->hasFile("avatar_image")){
+        if ($request->hasFile("avatar_image")) {
             $storagePath = config('app.client_avatar_storage_path');
-            Storage::disk("local")->delete("$storagePath"."/".$client->img_name);
-            $request->file("avatar_image")->storeAs($storagePath,$client->img_name,"local"); // NeedEdit: add env var
+            Storage::disk("local")->delete("$storagePath" . "/" . $client->img_name);
+            $request->file("avatar_image")->storeAs($storagePath, $client->img_name, "local"); // NeedEdit: add env var
         }
 
-        if($request->filled("password")){
-            $client->user->update(["password"=> Hash::make($request->password)]);
+        if ($request->filled("password")) {
+            $client->user->update(["password" => Hash::make($request->password)]);
         }
 
-        return back()->with("success","Client updated successfully");
+        return back()->with("success", "Client updated successfully");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id,Request $request)
+    public function destroy(string $id, Request $request)
     {
 
-            $client = Client::with("user")->findOrFail( $id );
-            $user = $client->user;
-            $client->delete();
-            $user->delete();
-            if ($client->img_name !== "default.jpg") {
-                $storagePath = config('app.client_avatar_storage_path');
-                Storage::disk("local")->delete($storagePath."/".$client->img_name);
-            }
-
-            //case the user deleted his own account
-            if(Auth::user()->id == $id)
-            {
-                Auth::guard('web')->logout();
-                $request->session()->invalidate();
-                $request->session()->regenerateToken();
-                return redirect('/');
-            }
-
-            //case an addmin or manager delted it
-            return back()->with("success","Client deleted successfully");
+        $client = Client::with("user")->findOrFail($id);
+        $user = $client->user;
+        $client->delete();
+        $user->delete();
+        if ($client->img_name !== "default.jpg") {
+            $storagePath = config('app.client_avatar_storage_path');
+            Storage::disk("local")->delete($storagePath . "/" . $client->img_name);
         }
+
+        //case the user deleted his own account
+        if (Auth::user()->id == $id) {
+            Auth::guard('web')->logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+            return redirect('/');
+        }
+
+        //case an addmin or manager delted it
+        return back()->with("success", "Client deleted successfully");
+    }
 
     /**
      * Approve a client account
      */
-    public function approve(string $id)
+    public function approve(Client $client)
     {
         // Ensure only authorized users can approve clients
         if (!Auth::check() || !Auth::user()->hasAnyPermission(['approve clients', 'manage clients'])) {
             return response()->json(['message' => 'Unauthorized'], 403);
         }
 
-        $client = Client::findOrFail($id);
-
         // Only update if not already approved
         if ($client->approved_by === null) {
             $client->update([
-                'approved_by' => Auth::id()
+                'approved_by' => Auth::id(),
             ]);
-
-            return back()->with('success', 'Client has been approved successfully');
+            $client->user->notify(new ClientApprovedNotification($client));
+            return back()->with('success', 'Client has been approved successfully and an email has been sent to the client.');
         }
 
         return back()->with('info', 'Client was already approved');
