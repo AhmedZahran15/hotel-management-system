e82a6ea24869b334ce2836cd29542e3fade03011
marriaammm <mariamz@std.mans.edu.eg>
2025-03-27 23:12:16 +0200

    feat: add Phone model, factory, and seeder for client phone numbers

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Phone extends Model
 {
+    use HasFactory;
     protected $table = 'clients_phones';
 
     protected $fillable = [
---
+++ database/factories/PhoneFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Client;
+use App\Models\Phone;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Phone>
+ */
+class PhoneFactory extends Factory
+{
+    protected $model = Phone::class;
+    public function definition(): array
+    {
+        return [
+            'client_id' => Client::inRandomOrder()->first()->id, 
+            'phone_number'  => $this->faker->phoneNumber,
+        ];
+    }
+}
