e54e4511f4b789d82a93b3f1158463215d3ee139
marriaammm <mariamz@std.mans.edu.eg>
2025-03-27 15:10:35 +0200

    refactor: remove ManagerReceptionistController and associated routes for cleaner codebase

--- routes/manager.php
+++ routes/manager.php
@@ -2,12 +2,9 @@
 
 use App\Http\Controllers\ClientExportController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ManagerReceptionistController;
 
 Route::middleware(['auth', 'verified', 'role:manager'])->prefix('dashboard/manager')->group(function () {
     Route::prefix('receptionists')->name('manager.receptionists.')->group(function () {
-        Route::post('{receptionist}/ban', [ManagerReceptionistController::class, 'ban'])->name('ban');
-        Route::post('{receptionist}/unban', [ManagerReceptionistController::class, 'unban'])->name('unban');
         Route::get('clients/export', [ClientExportController::class, 'export'])->name('clients.export');
     });
 });
