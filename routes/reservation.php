530f7ce182ca89a10957554c22cf884a34dd23f8
DaliaZH <121820334+AhmedZahran15@users.noreply.github.com>
2025-04-02 23:59:41 +0200

    refactor: remove unused payment success and cancel routes in reservation routes

--- routes/reservation.php
+++ routes/reservation.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReservationController;
-use App\Http\Middleware\CheckClientApproval;
 
 // Public Reservation Routes - Outside the dashboard
 Route::get('reservations/make', [ReservationController::class, 'makeReservation'])->name('reservations.make');
@@ -12,10 +11,4 @@ Route::middleware(['auth', 'verified', 'role:client'])
     ->prefix('dashboard')->group(function () {
         Route::post('reservations', [ReservationController::class, 'store'])->name('reservations.store');
         Route::get('reservations/rooms/{roomId}', [ReservationController::class, 'create'])->name('reservations.create');
-
-        Route::post('/reservations/payment/success', [ReservationController::class, 'paymentSuccess'])
-            ->name('reservations.payment.success');
-
-        Route::get('/reservations/payment/cancel', [ReservationController::class, 'paymentCancel'])
-            ->name('reservations.payment.cancel');
     });
