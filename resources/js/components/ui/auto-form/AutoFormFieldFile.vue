116e516f28fd861c79122ad21004654b80410099
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-04-03 23:00:25 +0200

    fix: update file input handling to store the file directly instead of base64 encoding

--- resources/js/components/ui/auto-form/AutoFormFieldFile.vue
+++ resources/js/components/ui/auto-form/AutoFormFieldFile.vue
@@ -11,64 +11,50 @@ import { beautifyObjectName } from './utils'
 defineProps<FieldProps>()
 
 const inputFile = ref<File>()
-async function parseFileAsString(file: File | undefined): Promise<string> {
-  return new Promise((resolve, reject) => {
-    if (file) {
-      const reader = new FileReader()
-      reader.onloadend = () => {
-        resolve(reader.result as string)
-      }
-      reader.onerror = (err) => {
-        reject(err)
-      }
-      reader.readAsDataURL(file)
-    }
-  })
-}
+
 </script>
 
 <template>
-  <FormField v-slot="slotProps" :name="fieldName">
+<FormField v-slot="slotProps" :name="fieldName">
     <FormItem v-bind="$attrs">
-      <AutoFormLabel v-if="!config?.hideLabel" :required="required">
-        {{ config?.label || beautifyObjectName(label ?? fieldName) }}
-      </AutoFormLabel>
-      <FormControl>
-        <slot v-bind="slotProps">
-          <Input
+        <AutoFormLabel v-if="!config?.hideLabel" :required="required">
+            {{ config?.label || beautifyObjectName(label ?? fieldName) }}
+        </AutoFormLabel>
+        <FormControl>
+            <slot v-bind="slotProps">
+            <Input
             v-if="!inputFile"
             type="file"
             v-bind="{ ...config?.inputProps }"
             :disabled="config?.inputProps?.disabled ?? disabled"
-            @change="async (ev: InputEvent) => {
-              const file = (ev.target as HTMLInputElement).files?.[0]
-              inputFile = file
-              const parsed = await parseFileAsString(file)
-              slotProps.componentField.onInput(parsed)
+            @change="(ev: InputEvent) => {
+                const file = (ev.target as HTMLInputElement).files?.[0]
+                inputFile = file
+                slotProps.componentField.onInput(file) // Store the file directly instead of base64
             }"
-          />
-          <div v-else class="flex h-10 w-full items-center justify-between rounded-md border border-input bg-transparent pl-3 pr-1 py-1 text-sm shadow-sm transition-colors">
+            />
+            <div v-else class="flex h-10 w-full items-center justify-between rounded-md border border-input bg-transparent pl-3 pr-1 py-1 text-sm shadow-sm transition-colors">
             <p>{{ inputFile?.name }}</p>
             <Button
-              :size="'icon'"
-              :variant="'ghost'"
-              class="h-[26px] w-[26px]"
-              aria-label="Remove file"
-              type="button"
-              @click="() => {
+                :size="'icon'"
+                :variant="'ghost'"
+                class="h-[26px] w-[26px]"
+                aria-label="Remove file"
+                type="button"
+                @click="() => {
                 inputFile = undefined
                 slotProps.componentField.onInput(undefined)
-              }"
+            }"
             >
-              <TrashIcon :size="16" />
+            <TrashIcon :size="16" />
             </Button>
-          </div>
-        </slot>
-      </FormControl>
-      <FormDescription v-if="config?.description">
-        {{ config.description }}
-      </FormDescription>
-      <FormMessage />
+            </div>
+            </slot>
+        </FormControl>
+        <FormDescription v-if="config?.description">
+            {{ config.description }}
+        </FormDescription>
+        <FormMessage />
     </FormItem>
-  </FormField>
+</FormField>
 </template>
