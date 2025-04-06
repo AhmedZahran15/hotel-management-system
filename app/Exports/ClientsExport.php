03d51e3b0cc1fe92c172e940ddbdfa35ae5d0a16
marriaammm <mariamz@std.mans.edu.eg>
2025-04-07 01:48:03 +0200

    fix: add conditional styling for 'Approved By' column in ClientsExport

--- app/Exports/ClientsExport.php
+++ app/Exports/ClientsExport.php
@@ -8,8 +8,11 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class ClientsExport implements FromCollection, WithHeadings, WithMapping, WithStyles
+class ClientsExport implements FromCollection, WithHeadings, WithMapping, WithStyles, WithEvents
 {
     public function collection()
     {
@@ -55,13 +58,36 @@ class ClientsExport implements FromCollection, WithHeadings, WithMapping, WithSt
     public function styles(Worksheet $sheet)
     {
         return [
-            // Style the entire first row (header row) as bold
             1 => [
                 'font' => [
                     'bold' => true,
                     'size' => 12,
                 ],
             ],
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                $worksheet = $event->sheet->getDelegate();
+                $highestRow = $worksheet->getHighestRow();
+
+                for ($row = 2; $row <= $highestRow; $row++) {
+                    $cellValue = $worksheet->getCell("F{$row}")->getValue();
+                    
+                    if ($cellValue === '"Not approved yet"') {
+                        $worksheet->getStyle("F{$row}")->getFill()
+                            ->setFillType(Fill::FILL_SOLID)
+                            ->getStartColor()->setRGB('FF0000'); 
+                    } else {
+                        $worksheet->getStyle("F{$row}")->getFill()
+                            ->setFillType(Fill::FILL_SOLID)
+                            ->getStartColor()->setRGB('90EE90'); 
+                    }
+                }
+            },
         ];
     }
 }
